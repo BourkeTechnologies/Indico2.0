40be558493279d6b724bb15464840510dd516ae4
rusithbourketechnologies <rusith@bourketechnologies.com>
2016-08-12 12:16:07 +0530

    add comment
    comment

--- Indico20.BusinessObjects/Repositories/Core/IMenuItemRepository.cs
+++ Indico20.BusinessObjects/Repositories/Core/IMenuItemRepository.cs
@@ -1,12 +1,16 @@
-﻿using Indico20.BusinessObjects.Base.Core;
-using Indico20.BusinessObjects.Objects;
-using Indico20.BusinessObjects.Procedures;
+﻿using Indico20.BusinessObjects.Objects.Implementation;
+using Indico20.BusinessObjects.Procedures.Implementation;
 using System.Collections.Generic;
 
 namespace Indico20.BusinessObjects.Repositories.Core
 {
     public interface IMenuItemRepository : IRepository<MenuItem>
     {
+        /// <summary>
+        /// Get MenuItems for given user role
+        /// </summary>
+        /// <param name="userRole"></param>
+        /// <returns></returns>
         IEnumerable<GetMenuItemsForUserRoleResult> ForUserRole(int userRole);
     }
 }
