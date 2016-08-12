fb9c9b21fa4d0258f21f8354dc652c9cd0df8b6c
rusithbourketechnologies <rusith@bourketechnologies.com>
2016-08-12 11:28:08 +0530

    delete

--- Indico20.BusinessObjects/Repositories/Core/IUserStatusRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Indico20.BusinessObjects.Base.Core;
-using Indico20.BusinessObjects.Objects;
-
-namespace Indico20.BusinessObjects.Repositories.Core
-{
-    public interface IUserStatusRepository:IRepository<UserStatus>
-    {
-    }
-}
